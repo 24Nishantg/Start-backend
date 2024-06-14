ef9ac38789af742217125b5bafc6f8fdf49f9e95
24Nishantg <100300377+24Nishantg@users.noreply.github.com>
2024-06-14 12:32:46 +0530

    ApiError nad apiRespone flie created

--- src/index.js
+++ src/index.js
@@ -9,7 +9,20 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.on("error", (error)=>{
+    console.log("ERRO:", error);
+    throw error
+  })
+
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`server is running at port: ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("MONGO db connection failed !!!", err);
+})
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
